0a2d58e6c4f7be5bc61506e23f23d1cb6f181111
Hey-Zeus-byte <heyzeus.1096@gmail.com>
2022-02-24 15:29:25 -0800

    added snakejs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './index.css';
 
 const App = () => {
+  state = {
+    snakeDots: [
+      [0,0],
+      [0,2]
+    ]
+  }
   return (
-    <div className='game-area'></div>
+    <div className='game-area'>
+      <div className='snake-dot' style={{top:0, left:0}}></div>
+      <div className='snake-dot' style={{top:0, left:"2%"}}></div>
+      <div className='snake-dot' style={{top:0, left:"4%"}}></div>
+    </div>
   )
 }
 
