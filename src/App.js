723458a054088868c036d0a3f41ba1d6cb86cdfc
Hey-Zeus-byte <heyzeus.1096@gmail.com>
2022-02-25 09:15:00 -0800

    Added code to Snake.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
+import React, {Component} from 'react';
+import Snake from './Snake';
+import Food from './Food';
 import './index.css';
 
-const App = () => {
+const getRandomCoords = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return (x,y)
+}
+
+class App extends Component {
   state = {
+    food: getRandomCoords(),
+    direction: 'RIGHT',
     snakeDots: [
       [0,0],
-      [0,2]
+      [2,0]
     ]
   }
+
+  componentDidMount() {
+    document.onkeydown = this.onKeyDown;
+  }
+
+  onKeyDown = (e) => {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 38:
+        this.setState({direction: 'UP'});
+      case 40:
+        this.setState({direction: 'DOWN'});
+      case 37:
+        this.setState({direction: 'LEFT'});
+      case 39:
+        this.setState({direction: 'RIGHT'});
+        break;
+    }
+  }
+
+  moveSnake = () +=>
+
+  render() {
   return (
     <div className='game-area'>
-      <div className='snake-dot' style={{top:0, left:0}}></div>
-      <div className='snake-dot' style={{top:0, left:"2%"}}></div>
-      <div className='snake-dot' style={{top:0, left:"4%"}}></div>
+      <Snake snakeDots={this.state.snakeDots}/>
+      <Food dot={this.state.food}/>
     </div>
   )
-}
+}}
 
 export default App;
